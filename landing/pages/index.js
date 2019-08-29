899ee751f9b3189315b8e1c09d53af7c571fb996
stan <st.kurilin@gmail.com>
2019-08-29 18:28:26 +0300

    Introduce links.

--- landing/pages/index.js
+++ landing/pages/index.js
@@ -1,6 +1,10 @@
+import Link from 'next/link';
+
 const Index = () => (
     <div>
         <p>AWS wizard landing</p>
+        <p>It's so cool.</p>
+        <Link href='/static-website'><p>GO STATIC</p></Link>
     </div>
 );
 
---
+++ landing/pages/static-website.js
@@ -0,0 +1,7 @@
+const Index = () => (
+    <div>
+        <p>I'm static</p>
+    </div>
+);
+
+export default Index;
