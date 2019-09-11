b0962f0397248b31df7c4a37e14d2b70b8cfefa0
stan <st.kurilin@gmail.com>
2019-09-11 14:39:25 +0300

    Publish new version to pypi.

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="awswizard",
-    version='0.0.1',
+    version='0.0.2',
     author="aws wizard",
     author_email="st.kurilin@gmail.com",
     description="Amazon made simple.",
